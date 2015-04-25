eac3ec476a7af66e5481f4acbea4bc5f68df4433
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2015-04-25 16:16:32 +0530

    Added shell file

---
+++ list-provisioning-profile-name.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+read -d '' script << 'EOF'
+BEGIN {
+    e = 1
+    pat = "<string>"tolower(provisioningProfile)
+}
+{
+    cur = tolower($0)
+    if (cur ~ pat && prev ~ /<key>name<\\/key>/) {
+        sub(/<string>/,"",cur)
+        sub(/<\\/string>/,"",cur)
+        print FILENAME cur
+        e = 0
+    }
+    if ($0 !~ /^\s*$/) {
+        prev = cur
+    }
+}
+END {
+ exit e
+}
+EOF
+
+
+awk -v "provisioningProfile=$1" "$script" *
